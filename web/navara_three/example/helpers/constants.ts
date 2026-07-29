62b9f2775c8dba6258ef475f782e68045d4f8bae
keiya01 <keiya.s.0210@gmail.com>
2026-07-29 14:48:38 +0900

    chore: update asset url

--- web/navara_three/example/helpers/constants.ts
+++ web/navara_three/example/helpers/constants.ts
@@ -351,34 +351,35 @@ export const GEOJSON_DATASETS = {
  */
 export const LOCAL_DATASETS = {
   blueMarbleClouds: {
-    url: "https://assets.cms.reearth.io/assets/c5/2b6941-2544-4cbd-9fcc-b245373269f9/blue-marble-clouds/{z}/{x}/{y}.webp",
+    url: "https://assets.cms.reearth.io/assets/11/ea0a6a-a94f-47e0-a163-4c675261a1f6/blue-marble-clouds/{z}/{x}/{y}.webp",
     attribution: "NASA Blue Marble Clouds(Converted as raster tiles)",
   },
   blueMarbleNight: {
-    url: "https://assets.cms.reearth.io/assets/5e/869e3a-8b89-401a-8bb3-580df6bfd0dc/blue-marble-night/{z}/{x}/{y}.webp",
-    attribution: "NASA Earth at Night imagery(Converted as raster tiles)",
+    url: "https://papers.reearth.land/blackmarble/{z}/{x}/{y}.webp",
+    attributionHtml:
+      '\u003Ca href="https://papers.reearth.land"\u003ERe:Earth Papers\u003C/a\u003E · \u003Ca href="https://science.nasa.gov/earth/earth-observatory/earth-at-night/maps"\u003ENASA Earth Observatory\u003C/a\u003E · Suomi NPP VIIRS · Black Marble 2016',
   },
   airportTrafficVolume: {
-    url: "https://assets.cms.reearth.io/assets/f9/0243d6-d330-43ca-9f42-2e91c181b5d1/airport-traffic-volume.geojson",
+    url: "https://assets.cms.reearth.io/assets/3b/63858d-9197-4b39-bbed-c17b6add52a4/airport-traffic-volume.geojson",
     attribution:
       "Processed and created from the National Land Numerical Information (inter-airport flow volume data) - MLIT",
     attributionUrl:
       "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-S10b-2014.html",
   },
   tokyoFlood: {
-    url: "https://assets.cms.reearth.io/assets/a8/1628e4-7a65-4737-b513-0d6fa20802f5/tokyo-flood.geojson",
+    url: "https://assets.cms.reearth.io/assets/11/55daed-a75e-4e8d-b021-c7fd0666e839/tokyo-flood.geojson",
     attribution: "Flood Inundation Zone Data (River-Based) - MLIT",
     attributionUrl:
       "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-A31a-2024.html",
   },
   railways: {
-    url: "https://assets.cms.reearth.io/assets/a6/783eed-3259-4de0-a5ca-58ae9e25af0d/railways.geojson",
+    url: "https://assets.cms.reearth.io/assets/51/794e3a-9464-4655-b96b-ff745781270b/railways.geojson",
     attribution: "Railway Data - MLIT",
     attributionUrl:
       "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N02-2024.html",
   },
   railwaysTimeSeries: {
-    url: "https://assets.cms.reearth.io/assets/05/3be5fb-214b-4914-998e-19e04881ef61/railwaysTimeSeries.geojson",
+    url: "https://assets.cms.reearth.io/assets/7c/f0b635-b985-491a-86c2-ff78b91f139b/railwaysTimeSeries.geojson",
     attribution: "Railway Time Series Data - MLIT",
     attributionUrl:
       "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N05-2024.html",
